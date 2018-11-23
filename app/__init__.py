929272040a38dd2a48c434bdb9d51aa854d51bdb
王翔 <xiang.wang2@dianrong.com>
2018-11-23 14:27:24 +0800

    db config

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     bootstrap.init_app(app)
 
